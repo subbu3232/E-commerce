4462cd12ba66f260125ea59831357eeaf1941154
subbu3232 <130164259+subbu3232@users.noreply.github.com>
2023-08-21 12:09:54 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -70,7 +70,10 @@ navigate(route.CART)
 
         } } >🛒</div>
 </div>
-
+<div className={styles.banner}>
+     {/* <h1>Hot Sale Going Live! Shop Now</h1> */}
+     <h1>The Sale Is On</h1>
+</div>
 <div className={styles.products}>
 
   {search === '' ? (
